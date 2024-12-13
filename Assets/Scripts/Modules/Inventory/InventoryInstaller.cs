12ed80e5d7eaae0387dcd1593811bf06b761c162
raketa420 <raketa420311@gmail.com>
2024-12-13 12:53:06 +0300

    invntory update

---
+++ Assets/Scripts/Modules/Inventory/InventoryInstaller.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using Zenject;
+
+public class InventoryInstaller : MonoInstaller
+{
+    [SerializeField]
+    private InventoryView _inventoryView;
+
+    [SerializeField]
+    private GameObject _slotViewPrefab;
+
+    public override void InstallBindings()
+    {
+        Container
+            .BindInterfacesAndSelfTo<InventoryPresenter>()
+            .AsSingle()
+            .WithArguments(new Inventory(10), _inventoryView, _slotViewPrefab)
+            .NonLazy();
+    }
+}
---
+++ Assets/Scripts/Modules/Inventory/InventorySlotView.cs
@@ -0,0 +1,49 @@
+﻿using TMPro;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InventorySlotView : MonoBehaviour
+{
+    [SerializeField]
+    private Button _slotButton;
+
+    [SerializeField]
+    private Image _itemImage;
+
+    [SerializeField]
+    private TMP_Text _itemNameText;
+
+    public void SetSlotItem(InventoryItem item)
+    {
+        if (!_itemImage.GameObject().gameObject.activeSelf)
+        {
+            EnableSlotItemView();
+
+            _itemImage.sprite = item.Icon;
+            _itemNameText.text = item.Name;
+        }
+    }
+
+    public void ClearSlot()
+    {
+        if (_itemImage.GameObject().gameObject.activeSelf)
+        {
+            DisableSlotItemView();
+        }
+    }
+
+    private void EnableSlotItemView()
+    {
+        _itemImage.GameObject().SetActive(true);
+        _itemNameText.GameObject().SetActive(true);
+        _slotButton.interactable = true;
+    }
+
+    private void DisableSlotItemView()
+    {
+        _itemImage.GameObject().SetActive(false);
+        _itemNameText.GameObject().SetActive(false);
+        _slotButton.interactable = false;
+    }
+}
